c342da9c9e179b083db5f9de81b2a50f3cb40fbd
fargusplumdoodle <fargusplumdoodle@gmail.com>
2023-02-20 16:59:54 -0600

    Built types from database

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,9 @@
-// TOOD: Generate this with python
-export type RuleSet = "Fantasy" | "DarkAges";
+import data from "./data.json";
+
+export type RuleSet = keyof typeof data.rulesets;
+export type FantasyFactions = keyof typeof data.rulesets.fantasy.factions;
+export type DarkAgesFaction = keyof typeof data.rulesets.dark_ages.factions;
+
+export type Weapons = keyof typeof data.weapons;
+export type Armour = keyof typeof data.armour;
+export type Abilities = keyof typeof data.abilities;
