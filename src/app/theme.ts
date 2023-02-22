9cb50f535a0b9f7eeeacb0d7f50298f4ef1794a8
miabrooker <brookermia@gmail.com>
2023-02-22 09:10:44 -0800

    set h3 (view headers) to antique roman

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,15 +1,31 @@
 import { createTheme } from "@mui/material";
+import AntiqueRoman from "../assets/fonts/AntiqueRoman.ttf";
 
 const theme = createTheme({
+  typography: {
+    h3: {
+      fontFamily: ["AntiqueRoman", "sans-serif"].join(",")
+    }
+  },
   palette: {
     mode: "dark",
     primary: {
-      main: "#FF8A3C",
+      main: "#FF8A3C"
     },
     secondary: {
-      main: "#F3F4F6",
-    },
+      main: "#F3F4F6"
+    }
   },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+      @font-face {
+        font-family: 'AntiqueRoman';
+        src: url(${AntiqueRoman}) format("truetype")
+    }
+      `
+    }
+  }
 });
 
 export default theme;
