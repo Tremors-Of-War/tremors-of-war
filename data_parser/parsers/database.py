02d108b70b15851d57f58a50b333cbfbc0733957
fargusplumdoodle <fargusplumdoodle@gmail.com>
2023-02-24 09:23:03 -0600

    Added all of the factions to the top level

--- data_parser/parsers/database.py
+++ data_parser/parsers/database.py
@@ -4,8 +4,19 @@ from constants import DATABASE
 from parsers.abilities import parse_abilities
 from parsers.armour import parse_armour
 from parsers.mounts import parse_mounts
-from parsers.ruleset import parse_rulesets
+from parsers.ruleset import parse_rulesets, RULESET
 from parsers.weapons import parse_weapons
+
+
+def get_factions_from_rulesets(rulesets):
+    return {**rulesets['fantasy']['factions'], **rulesets['dark_ages']['factions']}
+
+
+def get_units_from_factions(factions):
+    units = {}
+    for faction_units in list(factions.values()):
+        units = {**units, **faction_units}
+    return units
 
 
 def parse_database():
@@ -16,6 +27,8 @@ def parse_database():
     armour = parse_armour(db)
     mounts = parse_mounts(db)
     rulesets = parse_rulesets(db)
+    factions = get_factions_from_rulesets(rulesets)
+    units = get_units_from_factions(factions)
 
     return {
         "abilities": abilities,
@@ -23,4 +36,6 @@ def parse_database():
         "weapons": weapons,
         "mounts": mounts,
         "rulesets": rulesets,
+        "factions": factions,
+        "units": units,
     }
