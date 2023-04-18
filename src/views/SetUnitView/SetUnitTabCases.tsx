a40f8b46a8c2bf05ae6097e4829b0e85d67e0c09
fargusplumdoodle <fargusplumdoodle@gmail.com>
2023-04-17 20:24:30 -0700

    Fixed a few eslint errors

--- src/views/SetUnitView/SetUnitTabCases.tsx
+++ src/views/SetUnitView/SetUnitTabCases.tsx
@@ -32,6 +32,7 @@ const SetUnitTabCases: FunctionComponent<Props> = ({
     if (existingModel && existingModel.unit) {
       setSelectedUnit(existingModel.unit.name);
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
